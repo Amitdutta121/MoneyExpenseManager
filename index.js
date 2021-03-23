2635f597b6b3a48a4c3002b7d6a35866ed2eab18
@amitdutta121 <amitdutta121@gmail.com>
2021-03-24 03:38:28 +0600

    Redux-persist added.

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import {Provider} from "react-redux";
-import configureStore from "./src/redux/store/store";
+import {configureStore} from "./src/redux/store/store";
+import persistedStore from "./src/redux/store/store";
 
-const store = configureStore();
+import {PersistGate} from "redux-persist/es/integration/react"
+
+const store = configureStore;
 
 const ExpenseManager = () => {
     return(
         <Provider store={store}>
-            <App>
+            <PersistGate persistor={persistedStore} loading={null}>
+                <App>
 
 
-            </App>
+                </App>
+            </PersistGate>
+
         </Provider>
     )
 }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,25 @@
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
 import categoryReducer from "../Reducers/CategoryReducer";
+import {persistStore, persistReducer} from "redux-persist";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createLogger} from "redux-logger/src";
+
 
 const rootReducer = combineReducers({
     categoryReducer:categoryReducer
 })
-const configureStore = ()=> createStore(rootReducer)
 
-export default configureStore;
+
+let persistConfig ={
+    key:'root',
+    storage:AsyncStorage,
+    whitelist:['categoryReducer']
+}
+
+const persistedReducer = persistReducer(persistConfig , rootReducer)
+
+
+export let configureStore =  createStore(persistedReducer, applyMiddleware(createLogger()))
+const persistedStore = persistStore(configureStore)
+
+export default persistedStore;
